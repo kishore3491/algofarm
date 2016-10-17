cf0318472dcbab6327543385bf7a7cebce2c4c61
Kishore <kishore.meetme@gmail.com>
2016-10-17 13:28:01 -0500

    Create AtoI.py

---
+++ Strings/AtoI.py
@@ -0,0 +1,57 @@
+class AtoI(object):
+    def convert(self, str):
+        """
+        :type str: str
+        :rtype: int
+        """
+        # Group1 = +~/- sign
+        # Group2 = [0-9]+
+        # Group3 = . (if exists)
+        # Group4 = [0-9]+
+        validation = {45: True, 46: True, 32: True, -32: False}
+        res = 0
+        decRes = 0
+        decimal = False
+        negative = False
+        spaceExpired = False
+        numPos = 1
+        INT_MAX = 2147483647
+        
+        for c in reversed(str):
+            val = ord(c)
+            # Integer overflow check.
+            if numPos > 9 and val > 2:
+                return INT_MAX
+            
+            if val in validation:
+                # do something
+                if val == 46:
+                    # Set sign and space to false
+                    validation[45] = False
+                    validation[32] = False
+                    decimal = True
+                elif val == 45:
+                    validation[32] = False
+                    negative = True
+                
+                    
+            if val in range(48, 58):
+                # append
+                if decimal:
+                    # append after zeros
+                    decRes = res/numPos
+                    res = 0
+                else:
+                    res += (val - ord('0'))*(10**numPos-1)
+                numPos += 1
+            else:
+                return 0
+        
+        # If decimal, add decimal result.
+        if decimal:
+            res += decRes
+        
+        if negative:
+            return -res
+        else:
+            return res
