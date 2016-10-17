e2c48b580bc0fb5cce3be5fb1b9e29190bce370d
Kishore <kishore.meetme@gmail.com>
2016-10-17 14:19:42 -0500

    Update AtoI.py

--- Strings/AtoI.py
+++ Strings/AtoI.py
@@ -1,58 +1,91 @@
 
-def convert(str):
+def convert(word):
     """
     :type str: str
     :rtype: int
+    """
+    """
+    Step 1: Strip trailing and leading white spaces and
+            set spaceExpired to True
+    Step 2: Use a validation map to disallow duplicate pointers
+            like ., ' ', +/- etc. These values should be allowed only once.
+    Step 3: Check for decimal pointers
+    Step 4: Check for negative pointers
+    Step 5: Disallow any numbers after encounter with +/- sign.
     """
     # Group1 = +~/- sign
     # Group2 = [0-9]+
     # Group3 = . (if exists)
     # Group4 = [0-9]+
-    validation = {45: True, 46: True, 32: True, -32: False}
-    res = 0
-    decRes = 0
+
+    # Validation vector, true for allowed.
+    # 43 = +, 45 = -, 46 = ., 32 = ' '
+    validation = {43: True, 45: True, 46: True, 32: True}
+    res = 0.0
+    decRes = 0.0
     decimal = False
     negative = False
-    spaceExpired = False
     numPos = 0
+    zero = ord('0')
     INT_MAX = 2147483647
+    numsAllowed = True
 
-    for c in reversed(str):
+    # Remove leading and trailing white spaces.
+    i = 0
+    j = len(word) - 1
+    while (i < j):
+        if word[i] == ' ':
+            i += 1
+        elif word[j] == ' ':
+            j -= 1
+        else:
+            validation[32] = False
+            break;
+
+    for c in reversed(word[i:j+1]):
         val = ord(c)
         # Integer overflow check.
-        if numPos > 9 and val > 2:
+        if numPos > 9 and (val - zero) > 2:
             return INT_MAX
 
-        if val in validation:
-            # do something
+        # Check if validation passes.
+        if val in validation and validation[val] == True:
             if val == 46:
-                # Set sign and space to false
-                validation[45] = False
-                validation[32] = False
+                # Set sign allowed to false
+                validation[46] = False
                 decimal = True
             elif val == 45:
-                validation[32] = False
+                validation[45] = False
                 negative = True
+                numsAllowed = False
+            elif val == 32:
+                return 'Invalid space in between characters.'
+            elif val == 43:
+                validation[43] = False
+                numsAllowed = False
 
 
-        elif val in range(48, 58):
+        elif val in range(48, 58) and numsAllowed:
             # append
             if decimal:
                 # append after zeros
                 decRes = float(res/(10**numPos))
-                res = 0
+
+                # Important: Reset numPos to 0
+                numPos = 0
+                res = (val - zero)*(10**numPos)
                 # Reset decimal
                 decimal = False
-            else:
-                res += (val - ord('0'))*(10**numPos)
-            numPos += 1
-        else:
-            print ('Val out of range.')
-            return 0
 
-    # If decimal, add decimal result.
-    if decimal:
-        res += decRes
+            else:
+                res += (val - zero)*(10**numPos)
+            numPos += 1
+
+        else:
+            return 'Invalid'
+
+    # add decimal result, if any.
+    res += decRes
 
     if negative:
         return -res
@@ -60,4 +93,12 @@ def convert(str):
         return res
 
 if __name__ == "__main__":
-    print (convert('-144.60'))
+    print (convert(' -144.60  '))
+    print (convert(' 144.60  '))
+    print (convert(' -144.606'))
+    print (convert(' -144.60*'))
+    print (convert(' -144. 60  '))
+    print (convert(' -14+4.60  '))
+    print (convert('-144'))
+    print (convert('1.0'))
+    print (convert('1'))
