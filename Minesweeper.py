1218de90f2354eabdaf1ac98e83b84afd648d0fe
Kishore <kishore.meetme@gmail.com>
2018-02-09 00:06:35 -0600

    Update to fully working version.

--- Minesweeper.py
+++ Minesweeper.py
@@ -3,6 +3,10 @@ import sys
 
 
 class GameEngine:
+    """
+    A working, toy Minesweeper game that can be played on CLI.
+    https://en.wikipedia.org/wiki/Minesweeper_(video_game)
+    """
     def __init__(self, rows, cols, mines):
         self.rows = rows
         self.cols = cols
@@ -18,6 +22,12 @@ class GameEngine:
         return (int(choice[0]), int(choice[2]))
 
     def _create_board(self):
+        """
+        Using two 2D arrays - one for keeping track of board,
+        and other for keeping track of user activity.
+        I.e the second board tracks user's location visit activity,
+        while the first one is basically static.
+        """
         self.board = []
         for i in range(self.rows):
             self.board.append([0]*self.cols)
@@ -30,6 +40,9 @@ class GameEngine:
         self._compute_mine_scores(self._mineLoc)
 
     def _get_random_mine_locations(self):
+        """
+        Generate random pairs of numbers in current game box range.
+        """
         res = set()
         j = 0
         # Ignore duplicates and fill set with requires mine locations
@@ -43,6 +56,10 @@ class GameEngine:
         return res
 
     def _compute_mine_scores(self, mineLoc):
+        """
+        This is trivial; Just increment a location's mine score,
+        if it is in proximity of any mine.      
+        """
         for loc in mineLoc:     # for each loc
             for i in range(-1, 2):
                 for j in range(-1, 2):
@@ -51,6 +68,11 @@ class GameEngine:
     
 
     def _reveal_board(self, userLoc):
+        """
+        Check if user location is in range, not revealed already,
+        and if not adjacent to any mines, recursively expand the neighbors.
+        On each expansion, decrement reveal count.
+        """
         if (userLoc[0]) in range(0, self.rows) and (userLoc[1]) in range(0, self.cols) and not (self._reveal[userLoc[0]][userLoc[1]]):
                 self._reveal[userLoc[0]][userLoc[1]] = True
                 self.revealCount -= 1
@@ -78,13 +100,16 @@ class GameEngine:
         self._create_board()
         self.print_board()
 
-        while self.revealCount > self.mines: # TODO change to end game on success.
+        # If user has revealed all spots except for mines, he won.
+        while self.revealCount > self.mines:
             uc = self.getUserChoice()
+            # Kill game if user has landed on mine.
             if uc in self._mineLoc:
                 print("Stepped on mine. Boom. End of game.")
                 self.print_board()
                 print('Mine locations: ', self._mineLoc)
                 return
+            # Expand/Reveal board based on user's choice.
             self._reveal_board(uc)
             self.print_board()
         print("You Won!!")
